ad02d30414dcd8d034d45496f80d611d70f1bcef
NamNori-PHC <marc@thephoenixclub.com>
2025-04-25 07:53:37 -0700

    init

--- Pods/temp/NexusPod/include/sensor_bme280.h
+++
@@ -1,8 +0,0 @@
-#ifndef BME280_H
-#define BME280_H
-
-void sensor_bme280_setup(void);
-
-void sensor_bme280_measure(void);
-
-#endif
--- Pods/temp/NexusPod/include/sensor_sgp30.h
+++
@@ -1,8 +0,0 @@
-#ifndef SGP30_H
-#define SGP30_H
-
-void sensor_sgp30_setup(void);
-
-void sensor_sgp30_measure(void);
-
-#endif
